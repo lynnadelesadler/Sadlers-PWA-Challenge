e5e5606fbfad6b29286aef6c34ed7c473d34237b
lynnadelesadler <lynnadelesadler@yahoo.com>
2023-02-01 16:03:22 -0500

    update database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,8 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id,content) => {
-  console.log('Post to the database');
+export const putDb = async (id, content) => {
+  console.log('Put to the database');
    // Create a connection to the database database and version we want to use.
    const contactDb = await openDB('jate', 1);
   
@@ -25,7 +25,7 @@ export const putDb = async (id,content) => {
    const store = tx.objectStore('jate');
  
    // Use the .add() method on the store and pass in the content.
-   const request = store.put({ id: id, content: content});
+   const request = store.put({ id: id, jate: content});
  
    // Get confirmation of the request.
    const result = await request;
